33b016e7553edd67a404c9fb26f09d4edb3ea41e
Gavin Abeele <abeele.s.gavin@gmail.com>
2021-11-30 12:05:06 -0500

    Inital project creation and outline.

---
+++ Client/Source.c
@@ -0,0 +1,12 @@
+///CSCN72020 - Computer Networks Assignment 3 
+///RESTful API in C
+///
+///Gavin Abeele - gabeele2160@conestogac.on.ca
+/// 
+///v1.0 - December 1st, 2021: Inital project
+
+
+void main() {
+
+	return 0;
+}
